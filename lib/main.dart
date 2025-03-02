57c1aa16da8ecbfe59e0ce73212c9c5becc8b1c7
Nada Nabil <nadanabilhelmyy@gmail.com>
2025-03-03 01:40:09 +0200

    edited file

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:td_events_booking/core/di/dependency_injection.dart';
 import 'app.dart';
+import 'core/helpers/bloc_observer.dart';
+import 'core/routing/app_router.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  await ScreenUtil.ensureScreenSize();
+  setupGetIt();
+  Bloc.observer = MyBlocObserver();
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => App(
+        appRouter: AppRouter(),
+      ), // Wrap your app
+    ),
+  );
 }
-
-
-
