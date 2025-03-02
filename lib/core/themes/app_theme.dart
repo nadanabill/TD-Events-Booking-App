1aa0798f80b0061be9c6b5f44514486a1fb6823d
Nada Nabil <nadanabilhelmyy@gmail.com>
2025-03-03 01:32:15 +0200

    added core files

---
+++ lib/core/themes/app_theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'app_colors.dart';
+
+ThemeData appTheme() => ThemeData(
+      primaryColor: AppColors.primary,
+      appBarTheme: const AppBarTheme(
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarIconBrightness: Brightness.light,
+          statusBarBrightness: Brightness.light,
+          statusBarColor: Colors.transparent,
+        ),
+        backgroundColor: AppColors.white,
+        scrolledUnderElevation: 0,
+        elevation: 0.0,
+        shadowColor: AppColors.white,
+      ),
+      textSelectionTheme: const TextSelectionThemeData(
+        cursorColor: AppColors.primary,
+        selectionColor: AppColors.primary,
+        selectionHandleColor: AppColors.primary,
+      ),
+      scaffoldBackgroundColor: AppColors.white,
+    );
