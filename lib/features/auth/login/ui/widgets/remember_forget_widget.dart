d69c1f26c9b577bf022ade4c77c28037fa8088ab
Nada Nabil <nadanabilhelmyy@gmail.com>
2025-03-04 01:36:44 +0200

    added remember and forget widget

---
+++ lib/features/auth/login/ui/widgets/remember_forget_widget.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/constants/app_strings.dart';
+import '../../../../../core/themes/app_colors.dart';
+import '../../../../../core/themes/app_text_styles.dart';
+
+class RememberForgetWidget extends StatefulWidget {
+  const RememberForgetWidget({super.key});
+
+  @override
+  State<RememberForgetWidget> createState() => _RememberForgetWidgetState();
+}
+
+class _RememberForgetWidgetState extends State<RememberForgetWidget> {
+  bool rememberMe = true;
+  @override
+  Widget build(BuildContext context) {
+    return  Row(
+      children: [
+        Switch(
+            inactiveThumbColor: AppColors.primary,
+            inactiveTrackColor: AppColors.white,
+            activeColor: AppColors.white,
+            activeTrackColor: AppColors.primary,
+            value: rememberMe,
+            onChanged: (value) {
+              rememberMe = !rememberMe;
+              setState(() {});
+            }),
+        Text(
+          AppStrings.rememberMe,
+          style: AppTextStyles.font14Black400,
+        ),
+        const Spacer(),
+        TextButton(
+          onPressed: () {},
+          child: Text(
+            AppStrings.forgotPassword,
+            style: AppTextStyles.font14Black400,
+          ),
+        ),
+      ],
+    );
+  }
+}
