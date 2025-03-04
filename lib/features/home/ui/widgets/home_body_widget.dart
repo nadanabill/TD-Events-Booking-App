4342d7454c12e678ca66bd537bfd04ac81703d0a
Nada Nabil <nadanabilhelmyy@gmail.com>
2025-03-05 01:31:54 +0200

    fixed layout

--- lib/features/home/ui/widgets/home_body_widget.dart
+++ lib/features/home/ui/widgets/home_body_widget.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:td_events_booking/features/home/ui/widgets/categories_list_widget.dart';
 
 import '../../../../core/constants/app_strings.dart';
+import '../../../../core/themes/app_colors.dart';
 import 'events_list_widget.dart';
+import 'home_app_bar_widget.dart';
 import 'invite_card_widget.dart';
 import 'text_row_widget.dart';
 
@@ -10,13 +14,31 @@ class HomeBodyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        TextRowWidget(text: AppStrings.upcomingEvents),
-        const EventsListWidget(),
-        const InviteCardWidget(),
-        TextRowWidget(text: AppStrings.nearbyEvents),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.primary,
+        toolbarHeight: 90.h,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(33),
+          ),
+        ),
+        flexibleSpace: const SafeArea(
+          child: HomeAppBarWidget(),
+        ),
+        bottom: const PreferredSize(
+          preferredSize: Size.fromHeight(40),
+          child: CategoriesListWidget(),
+        ),
+      ),
+      body: ListView(
+        children: [
+          TextRowWidget(text: AppStrings.upcomingEvents),
+          const EventsListWidget(),
+          const InviteCardWidget(),
+          TextRowWidget(text: AppStrings.nearbyEvents),
+        ],
+      ),
     );
   }
 }
