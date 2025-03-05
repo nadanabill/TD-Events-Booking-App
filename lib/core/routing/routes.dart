1f1d6b7757b7a752ab649e6b1a83a1a6a01e6507
Nada Nabil <nadanabilhelmyy@gmail.com>
2025-03-05 06:35:42 +0200

    added event details screen

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -4,4 +4,5 @@ class Routes {
   static const String registerScreen = "/registerScreen";
   static const String otpScreen = "/otpScreen";
   static const String layout = "/layout";
+  static const String eventDetailsScreen = "/eventDetailsScreen";
 }
