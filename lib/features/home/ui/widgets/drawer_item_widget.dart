5aeeed6c2aced649e1d12f2976eb6eeb370a940a
Nada Nabil <nadanabilhelmyy@gmail.com>
2025-03-06 23:27:48 +0200

    added drawer item widget

---
+++ lib/features/home/ui/widgets/drawer_item_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:td_events_booking/features/home/data/models/drawer_model.dart';
+
+import '../../../../core/helpers/spaces.dart';
+import '../../../../core/themes/app_text_styles.dart';
+
+class DrawerItemWidget extends StatelessWidget {
+  final DrawerModel drawerItem;
+
+  const DrawerItemWidget({
+    super.key,
+    required this.drawerItem,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, drawerItem.router);
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SvgPicture.asset(drawerItem.icon),
+          horizontalSpace(10),
+          Text(
+            drawerItem.title,
+            style: AppTextStyles.font16Black400,
+          )
+        ],
+      ),
+    );
+  }
+}
