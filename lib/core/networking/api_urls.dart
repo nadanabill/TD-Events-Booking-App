f1f84ac536a13f0166e84f7bd0868eeee0ecfc1a
Nada Nabil <nadanabilhelmyy@gmail.com>
2025-03-07 23:12:35 +0200

    added all events url

--- lib/core/networking/api_urls.dart
+++ lib/core/networking/api_urls.dart
@@ -1,4 +1,5 @@
 class ApiUrls {
   static const String baseUrl = "http://api.td-dev.turndigital.net/api/";
   static const String register = "users/register";
+  static const String allEvents = "api/events";
 }
