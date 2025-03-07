494058e16d15f30b62084cf8651e8c58c618c246
Nada Nabil <nadanabilhelmyy@gmail.com>
2025-03-07 23:15:24 +0200

    added all events api service

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -4,6 +4,7 @@ import 'package:td_events_booking/features/auth/login/logic/login_cubit.dart';
 import 'package:td_events_booking/features/auth/register/data/api/register_api_service.dart';
 import 'package:td_events_booking/features/auth/register/logic/register_cubit.dart';
 
+import '../../features/all_events/data/api/all_events_api_service.dart';
 import '../../features/auth/register/data/repo/register_repo.dart';
 import '../networking/dio_factory.dart';
 
@@ -22,4 +23,8 @@ Future<void> setupGetIt() async {
 
   // login Feature
   getIt.registerLazySingleton<LoginCubit>(() => LoginCubit());
+
+  // all events Feature
+  getIt.registerLazySingleton<AllEventsApiService>(
+      () => AllEventsApiService(dio));
 }
