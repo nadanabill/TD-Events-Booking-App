aa96b08b8c9a36d78e52281d2c1e8a18a684a889
Nada Nabil <nadanabilhelmyy@gmail.com>
2025-03-08 19:19:08 +0200

    added organizer profile url and edited events url

--- lib/core/networking/api_urls.dart
+++ lib/core/networking/api_urls.dart
@@ -1,5 +1,6 @@
 class ApiUrls {
   static const String baseUrl = "http://api.td-dev.turndigital.net/api/";
   static const String register = "users/register";
-  static const String allEvents = "api/events";
+  static const String allEvents = "events";
+  static const String organizers = 'organizers';
 }
