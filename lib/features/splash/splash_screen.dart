f89cbb7dea3c4d693c2d3a395002e8f9284f6540
Nada Nabil <nadanabilhelmyy@gmail.com>
2025-03-08 21:46:26 +0200

    added native splash and app icon

---
+++ lib/features/splash/splash_screen.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:td_events_booking/core/routing/routes.dart';
+
+import '../../core/constants/app_assets.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushNamedAndRemoveUntil(
+        context,
+        Routes.loginScreen,
+        (route) => false,
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(AppImages.background),
+            fit: BoxFit.fill,
+          ),
+        ),
+        alignment: Alignment.center,
+        child: Image.asset(
+          'assets/images/logo.png',
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
