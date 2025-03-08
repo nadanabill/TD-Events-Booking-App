a7c9d8478e47f3f906246078ca81228012a882f2
Nada Nabil <nadanabilhelmyy@gmail.com>
2025-03-08 21:48:28 +0200

    edited splash screen

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,4 +1,5 @@
 class Routes {
+  static const String splashScreen = "/splashScreen";
   static const String onboardingScreen = "/onboardingScreen";
   static const String loginScreen = "/loginScreen";
   static const String registerScreen = "/registerScreen";
