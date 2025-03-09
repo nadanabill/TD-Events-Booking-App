abc0eb1a7064d59dffc6a3176c0ba3fe519233c9
Nada Nabil <nadanabilhelmyy@gmail.com>
2025-03-09 19:42:14 +0200

    added local notification service

---
+++ lib/core/helpers/network_helper.dart
@@ -0,0 +1,14 @@
+// import 'package:connectivity_plus/connectivity_plus.dart';
+//
+// class NetworkHelper {
+//   final Connectivity _connectivity = Connectivity();
+//
+//   Future<bool> hasConnection() async {
+//     final result = await _connectivity.checkConnectivity();
+//     _connectivitySubscription =
+//         Connectivity().onConnectivityChanged.listen((ConnectivityResult result){
+//
+//     }
+//     return false;
+//   }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,13 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:td_events_booking/core/di/dependency_injection.dart';
 import 'app.dart';
 import 'core/helpers/bloc_observer.dart';
+import 'core/helpers/notification_service.dart';
 import 'core/routing/app_router.dart';
 
 void main() async {
   await ScreenUtil.ensureScreenSize();
   setupGetIt();
+  await NotificationService().init();
   Bloc.observer = MyBlocObserver();
   runApp(
     DevicePreview(
