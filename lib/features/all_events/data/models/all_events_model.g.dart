03fa7a0030ce2e6fbce3531a1cc021138ab78ec2
Nada Nabil <nadanabilhelmyy@gmail.com>
2025-03-09 08:06:05 +0200

    edited event model

--- lib/features/all_events/data/models/all_events_model.g.dart
+++ lib/features/all_events/data/models/all_events_model.g.dart
@@ -13,30 +13,27 @@ AllEventsModel _$AllEventsModelFromJson(Map<String, dynamic> json) =>
       data: Data.fromJson(json['data'] as Map<String, dynamic>),
     );
 
+Map<String, dynamic> _$AllEventsModelToJson(AllEventsModel instance) =>
+    <String, dynamic>{
+      'success': instance.success,
+      'message': instance.message,
+      'data': instance.data,
+    };
+
 Data _$DataFromJson(Map<String, dynamic> json) => Data(
       currentPage: (json['current_page'] as num?)?.toInt(),
       pageSize: (json['page_size'] as num?)?.toInt(),
       totalItems: (json['total_items'] as num?)?.toInt(),
       totalPages: (json['total_pages'] as num?)?.toInt(),
       events: (json['events'] as List<dynamic>?)
-          ?.map((e) => Events.fromJson(e as Map<String, dynamic>))
+          ?.map((e) => Event.fromJson(e as Map<String, dynamic>))
           .toList(),
     );
 
-Events _$EventsFromJson(Map<String, dynamic> json) => Events(
-      eventId: (json['event_id'] as num?)?.toInt(),
-      picture: json['picture'] as String?,
-      date: json['date'] as String?,
-      title: json['title'] as String?,
-      address: json['address'] as String?,
-      numberOfGoing: (json['number_of_going'] as num?)?.toInt(),
-      organizer: json['organizer'] == null
-          ? null
-          : Organizer.fromJson(json['organizer'] as Map<String, dynamic>),
-    );
-
-Organizer _$OrganizerFromJson(Map<String, dynamic> json) => Organizer(
-      id: (json['id'] as num?)?.toInt(),
-      name: json['name'] as String?,
-      picture: json['picture'] as String?,
-    );
+Map<String, dynamic> _$DataToJson(Data instance) => <String, dynamic>{
+      'current_page': instance.currentPage,
+      'page_size': instance.pageSize,
+      'total_items': instance.totalItems,
+      'total_pages': instance.totalPages,
+      'events': instance.events,
+    };
--- lib/features/event/data/models/event_details_model.dart
+++ lib/features/event/data/models/event_details_model.dart
@@ -1,12 +1,14 @@
 import 'package:json_annotation/json_annotation.dart';
 
+import '../../../../core/models/event_model.dart';
+
 part 'event_details_model.g.dart';
 
 @JsonSerializable()
 class EventDetailsModel {
   bool? success;
   String? message;
-  EventData? data;
+  Event? data;
 
   EventDetailsModel({
     this.success,
@@ -16,59 +18,4 @@ class EventDetailsModel {
 
   factory EventDetailsModel.fromJson(Map<String, dynamic> json) =>
       _$EventDetailsModelFromJson(json);
-}
-
-@JsonSerializable()
-class EventData {
-  @JsonKey(name: 'event_id')
-  int? eventId;
-  String? picture;
-  String? date;
-  String? title;
-  String? address;
-  @JsonKey(name: 'number_of_going')
-  int? numberOfGoing;
-  @JsonKey(name: 'address_title')
-  String? addressTitle;
-  String? latitude;
-  String? longitude;
-  @JsonKey(name: 'about_event')
-  String? aboutEvent;
-  @JsonKey(name: 'event_price')
-  String? eventPrice;
-  Organizer? organizer;
-
-  EventData({
-    this.eventId,
-    this.picture,
-    this.date,
-    this.title,
-    this.address,
-    this.numberOfGoing,
-    this.addressTitle,
-    this.latitude,
-    this.longitude,
-    this.aboutEvent,
-    this.eventPrice,
-    this.organizer,
-  });
-
-  factory EventData.fromJson(Map<String, dynamic> json) =>
-      _$EventDataFromJson(json);
-}
-
-@JsonSerializable()
-class Organizer {
-  int? id;
-  String? name;
-  String? picture;
-
-  Organizer({
-    this.id,
-    this.name,
-    this.picture,
-  });
-
-  factory Organizer.fromJson(Map<String, dynamic> json) =>
-      _$OrganizerFromJson(json);
 }
